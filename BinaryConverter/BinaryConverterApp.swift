9358bfe70960976228653a09098a36a522a4432f
Louis <me@louisgallet.fr>
2023-11-04 14:03:54 +0100

    Initial Commit

---
+++ BinaryConverter/BinaryConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryConverterApp.swift
+//  BinaryConverter
+//
+//  Created by Louis Gallet on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BinaryConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
